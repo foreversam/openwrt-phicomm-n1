b09bbc1096f954b606aa1ace2101b705bf827988
foreversam <69178667+foreversam@users.noreply.github.com>
2021-06-18 02:36:31 +0000

    Update workflow

--- custom.sh
+++ custom.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.2/g' ./package/base-files/files/bin/config_generate
+
+# Clone additional packages
+git clone --depth 1 https://github.com/tuanqing/install-program.git ./package/install-program
