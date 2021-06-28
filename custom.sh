6daae9f12e2b6affd68e16b234a29474c7c94907
foreversam <69178667+foreversam@users.noreply.github.com>
2021-06-28 03:14:17 +0000

    Update config

--- custom.sh
+++ custom.sh
@@ -5,3 +5,4 @@ sed -i 's/192.168.1.1/192.168.1.2/g' ./package/base-files/files/bin/config_gener
 
 # Clone additional packages
 git clone --depth 1 https://github.com/tuanqing/install-program.git ./package/install-program
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git ./package/openwrt-passwall
