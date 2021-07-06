53a8df89876245364f7e1c731c9d028bc584bfc2
foreversam <69178667+foreversam@users.noreply.github.com>
2021-07-06 07:04:51 +0000

    Create new branch for openwrt/openwrt

--- custom.sh
+++ custom.sh
@@ -5,4 +5,3 @@ sed -i 's/192.168.1.1/192.168.1.2/g' ./package/base-files/files/bin/config_gener
 
 # Clone additional packages
 git clone --depth 1 https://github.com/tuanqing/install-program.git ./package/install-program
-git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git ./package/openwrt-passwall
